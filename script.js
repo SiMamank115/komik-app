9afb5eb1b9830d708940161cf5b6dbd35049d8f0
SiMamank115 <faizfullafalisa@gmail.com>
2022-12-12 14:54:52 +0700

    Pagination

--- script.js
+++ script.js
@@ -1,124 +1,153 @@
-// Import the functions you need from the SDKs you need
+// Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
 import { getDatabase, ref, set, onValue, update, remove, onChildChanged } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js";
 
-let db,
-    app,
-    datafetched = false,
-    dpp = 15,
-    dataStart = 0,
-    onToggleForm = false,
-    datas,
-    datasLength,
-    comicsList;
-if (getReq("pages") > 0) {
-    dataStart = (getReq("pages") - 1) * dpp;
-}
-let dataEnd = dataStart + dpp;
-fetch("./config.json")
-    .then((e) => e.json())
-    .then(async (e) => {
-        app = initializeApp(e.config);
-        db = getDatabase();
-        const dbRef = ref(db);
-        onChildChanged(dbRef, (data) => {
-            console.log(data.val());
-            printData(data.val());
-        });
-        onValue(
-            dbRef,
-            (snapshot) => {
-                snapshot.forEach((childSnapshot) => {
-                    const childKey = childSnapshot.key;
-                    const childData = childSnapshot.val();
-                    if (childKey == "list") {
-                        datas = childData;
-                        datasLength = Object.keys(childData).length;
-                        printData(childData);
-                        comicsList = new List(document.querySelector(".comics"), {
-                            valueNames: ["list-title", "list-type", "list-chapter"],
-                        });
-                    }
-                });
-            },
-            {
-                onlyOnce: true,
+// Variables
+let db, // firebase
+    app, // firebase
+    datafetched = false, // loading screen
+    dpp = 15, // pagination (data/page)
+    dataStart = 0, // pagination
+    dataEnd, // pagination
+    page = 1, // pagination
+    onToggleForm = false, // form animation
+    datas, // comics
+    comicsList; // comics
+document.addEventListener(
+    "DOMContentLoaded",
+    function () {
+        updatePage();
+        document.querySelector("#data-form").addEventListener("submit", (e) => {
+            let title = document.querySelector("#input-title>input").value;
+            let chapter = document.querySelector("#input-chapter>input").value;
+            let type = document.querySelector("#input-type>select").value;
+            let url = document.querySelector("#input-url>input").value;
+            if (e.target.dataset.action == "create") {
+                writeData(uuid("comic"), title, type, chapter, url);
+            } else if (e.target.dataset.action == "edit") {
+                updateData(e.target.dataset.target, title, type, chapter, url);
             }
-        );
-        return;
-    });
+            toggleForm("close");
+            e.preventDefault();
+            return false;
+        });
+        fetch("./config.json")
+            .then((e) => e.json())
+            .then(async (e) => {
+                app = initializeApp(e.config);
+                db = getDatabase();
+                const dbRef = ref(db);
+                onChildChanged(dbRef, (data) => {
+                    datas = data.val();
+                    printData(data.val());
+                });
+                onValue(
+                    dbRef,
+                    (snapshot) => {
+                        snapshot.forEach((childSnapshot) => {
+                            const childKey = childSnapshot.key;
+                            const childData = childSnapshot.val();
+                            if (childKey == "list") {
+                                datas = childData;
+                                printData(childData);
+                                comicsList = new List(document.querySelector(".comics"), {
+                                    page:15,
+                                    pagination:true,
+                                    valueNames: ["list-url", "list-type", "list-chapter"],
+                                });
+                                window.comic = comicsList;
+                            }
+                        });
+                    },
+                    {
+                        onlyOnce: true,
+                    }
+                );
+                return;
+            });
+    },
+    false
+);
+
+// Helper Functions
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+function getReq(name) {
+    if ((name = new RegExp("[?&]" + encodeURIComponent(name) + "=([^&]*)").exec(location.search))) return decodeURIComponent(name[1]);
+}
+function uuid(start = "id") {
+    return start + "-" + Math.random().toString(16).slice(2);
+}
+function updatePage() {
+    dataStart = (page - 1) * dpp;
+    dataEnd = dataStart + dpp;
+    Array.from(document.querySelector(".list").children).forEach((e,x) => {
+        if(x < dataStart || x > dataEnd) {
+            e.remove()
+        }
+    })
+}
+// Firebase CRUD Functions
 function writeData(id, title, type, chapter, url) {
     set(ref(db, "list/" + id), {
         title: title,
         type: type,
         chapter: chapter,
         url: url,
-    })
-        .then((e) => {
-            console.log("setted");
-        })
-        .catch((e) => {
-            console.log(e);
-        });
+    });
 }
-window.wd = writeData;
 function updateData(id, title, type, chapter, url) {
     let updates = {};
     updates["/list/" + id] = { title, type, chapter, url };
-    update(ref(db), updates)
-        .then((e) => {
-            console.log("editted");
-        })
-        .catch((e) => {
-            console.log(e);
-        });
+    update(ref(db), updates);
 }
 function deleteData(id) {
     remove(ref(db, "list/" + id));
 }
+// UI Functions
 function printData(data = [{ chapter: 0, title: "", url: "" }]) {
     let tbody = ``,
         keys = Object.keys(data);
-    keys = keys.slice(dataStart, dataEnd);
     keys.forEach((e, x) => {
-        let tr = `<tr class="hover:bg-white/5 transition-all">
-        <td class="list-titl data-table-td"><a class="list-url break-word hover:text-slate-100 transition-all w-full" href="${data[e].url}" style="word-break: break-word">${data[e].title}</a></td>
+        let tr = `<tr class="hover:bg-white/5 transition-all md:border-0 border-b-2 border-slate-500">
+        <td class="list-title data-table-td"><a class="list-url break-word hover:text-slate-100 transition-all w-full" target="_blank" href="${data[e].url}" style="word-break: break-word">${data[e].title}</a></td>
         <td class="list-type data-table-td">${data[e].type}</td>
         <td class="list-chapter data-table-td">${data[e].chapter}</td>
         <td class="data-table-td">
-            <button data-id ="${e}" id="edit" role="button" class="action-button hover:bg-blue-700 bg-blue-600"><i class="fa-regular fa-pen-to-square"></i></button>
-            <button data-id ="${e}" id="delete" role="button" class="action-button hover:bg-red-700 bg-red-600"><i class="fa-regular fa-trash-can"></i></button>
+            <button data-id ="${e}" id="edit" role="button" class="action-button hover:bg-blue-700 bg-blue-600 text-xs"><i class=" w-[12px] h-[12px] fa-regular fa-pen-to-square"></i></button>
+            <button data-id ="${e}" id="delete" role="button" class="action-button hover:bg-red-700 bg-red-600 text-xs"><i class=" w-[12px] h-[12px] fa-regular fa-trash-can"></i></button>
         </td>
     </tr>`;
-        tbody += tr
+        tbody += tr;
     });
     document.querySelector("table#data-table>tbody").innerHTML = tbody;
     if (!datafetched) {
-        document.querySelector("#loading-screen").classList.add("hidden");
+        if (document.querySelector("#loading-screen")) document.querySelector("#loading-screen").classList.add("hidden");
         document.querySelector("#content-wrapper").classList.remove("h-[100vh]");
         document.querySelector("#data-table").classList.remove("hidden");
         document.querySelector(".control-button").classList.remove("hidden");
+        document.querySelector(".control-button").classList.add("flex");
         document.querySelector(".form-control").classList.remove("hidden");
+        if (document.querySelector("#loading-screen")) document.querySelector("#loading-screen").remove();
     }
-
-    document.querySelector("button#create").addEventListener("click", (e) => {
+    document.querySelectorAll("button#delete").forEach((e) => {
+        e.addEventListener("dblclick", (e) => {
+            deleteData(e.currentTarget.dataset.id);
+        });
+    });
+    document.querySelector("button#create").onclick =  (e) => {
         createForm();
-    });
-    document.querySelector("button#close").addEventListener("click", (e) => {
+    };
+    document.querySelector("button#close").onclick =  (e) => {
         toggleForm("close");
-    });
+    };
     document.querySelectorAll("button#edit").forEach((e) => {
         e.addEventListener("click", (r) => {
             editForm(e.dataset.id);
         });
     });
     if (comicsList) comicsList.reIndex();
-}
-function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-}
-function getReq(name) {
-    if ((name = new RegExp("[?&]" + encodeURIComponent(name) + "=([^&]*)").exec(location.search))) return decodeURIComponent(name[1]);
 }
 async function toggleForm(action = "create", callback = function (target = document.querySelector(".form-control")) {}) {
     if ((action != "create" && action != "edit" && action != "close") || onToggleForm) return false;
@@ -168,20 +197,3 @@ function editForm(index = "") {
         inputs.querySelector("#input-submit").querySelector("button").textContent = "Edit";
     });
 }
-window.toggleForm = toggleForm;
-window.createForm = createForm;
-window.editForm = editForm;
-
-document.querySelector("#data-form").addEventListener("submit", (e) => {
-    let title = document.querySelector("#input-title>input").value;
-    let chapter = document.querySelector("#input-chapter>input").value;
-    let type = document.querySelector("#input-type>select").value;
-    let url = document.querySelector("#input-url>input").value;
-    if (e.target.dataset.action == "create") {
-        writeData("comic-" + datasLength, title, type, chapter, url);
-    } else if (e.target.dataset.action == "edit") {
-        updateData(e.target.dataset.target, title, type, chapter, url);
-    }
-    e.preventDefault();
-    return false;
-});
