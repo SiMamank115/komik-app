aa890397bf2aaf064a4a9217aafe0b53434b2017
Faeest <faeest.115@gmail.com>
2023-10-03 10:34:07 +0700

    release

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,14 @@
-module.export = {};
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries'),
+  ],
+}
+
